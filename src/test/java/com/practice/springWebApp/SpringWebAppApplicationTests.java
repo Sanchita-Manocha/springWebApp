27b1ea00a90315aa53a9663a56bad661e350a8fe
Sanchita <sanchita.manocha@afterpaytouch.com>
2021-09-29 06:49:32 +1000

    first commit

---
+++ src/test/java/com/practice/springWebApp/SpringWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.springWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
